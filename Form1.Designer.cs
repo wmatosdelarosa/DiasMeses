06c1191b2488171571b2d7819685c3b294f00533
Richard mora <rjmoraaraujo@gmail.com>
2018-08-15 00:50:51 +1200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace DiasMeses
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtDias = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMes = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.button1.Location = new System.Drawing.Point(430, 87);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Respuesta";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtDias
+            // 
+            this.txtDias.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txtDias.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDias.Location = new System.Drawing.Point(404, 45);
+            this.txtDias.Name = "txtDias";
+            this.txtDias.Size = new System.Drawing.Size(138, 22);
+            this.txtDias.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(114, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(214, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ingrese un numero del 1 A 7";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(105, 153);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(223, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Ingrese un numero del 1 A 12";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtMes
+            // 
+            this.txtMes.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txtMes.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMes.Location = new System.Drawing.Point(404, 148);
+            this.txtMes.Name = "txtMes";
+            this.txtMes.Size = new System.Drawing.Size(138, 22);
+            this.txtMes.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.button2.Location = new System.Drawing.Point(433, 189);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "VerRespuesta";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(307, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Dias y Meses";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button3.Location = new System.Drawing.Point(443, 218);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(56, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "salir";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(683, 253);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtMes);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDias);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtDias;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMes;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DiasMeses
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int dia;
+            dia = Convert.ToInt32(txtDias.Text);
+            if (dia > 0 && dia <= 7)
+            {
+                if (dia == 1)
+                    label1.Text =( "Lunes");
+                if (dia == 2)
+                    label1.Text = "Martes";
+                if (dia == 3)
+                    label1.Text = "Miercoles";
+                if (dia == 4)
+                    label1.Text = "Jueves" ;
+                if (dia == 5)
+                    label1.Text = "Viernes";
+                if (dia == 6)
+                    label1.Text = "Sabado";
+                if (dia == 7)
+                    label1.Text = "Domingo";
+            }
+
+            else
+            {
+                label1.Text = "Incorrecto";
+            }
+
+      
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            int mes;
+            mes = Convert.ToInt32(txtMes.Text);
+            if (mes > 0 && mes <= 12)
+            {
+                if (mes == 1)
+                    label1.Text = "Enero";
+
+                if (mes == 2)
+                    label1.Text = "Febrero";
+
+                if (mes == 3)
+                    label1.Text = "Marzo";
+                if (mes == 4)
+                    label1.Text = "Abril";
+                if (mes == 5)
+                    label1.Text = "Mayo";
+                if (mes == 6)
+                    label1.Text = "Junio";
+                if (mes == 7)
+                    label1.Text = "Julio";
+                if (mes == 8)
+                    label1.Text = "Agosto";
+                if (mes == 9)
+                    label1.Text = "Septiembre";
+                if (mes == 10)
+                    label1.Text = "Octubre";
+                if (mes == 11)
+                    label1.Text = "Noviembre";
+                if (mes == 12)
+                    label1.Text = "Diciembre";
+            }
+
+            else
+                label1.Text = "INCORRECTO";
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
+    
+
